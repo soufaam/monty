254551685c0e4b9de839c4aab1ee0fa6bbb547e1
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-27 01:01:02 +0100

    handling when unknow instruction

--- error_handling.c
+++ error_handling.c
@@ -19,7 +19,7 @@ void error_handling(int argc, char **argv, FILE **file)
 	fil = fopen(argv[1], "r");
 	if (!fil)
 	{
-		write(STDERR_FILENO, "Error: Can't open file",
+		write(STDERR_FILENO, "Error: Can't open file ",
 		_strlen("Error: Can't open file "));
 		write(STDERR_FILENO, argv[1], _strlen(argv[1]));
 		write(STDERR_FILENO, "\n", _strlen("\n"));
