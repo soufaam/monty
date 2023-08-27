d325f6eab62f39be1cab7fba42ffd7e6cb114566
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-08-27 01:42:26 +0100

    handling memory leak

--- exec_cmd.c
+++ exec_cmd.c
@@ -1,5 +1,39 @@
 #include "monty.h"
 
+
+/**
+* print_push_error - ENTRYPOINT
+* @line_number: third param
+* @cmd: command
+* @h: header
+* Return: 0 success or uint
+*/
+print_cmd_notfound(int line_number, char **cmd, stack_t *h)
+{
+		write(STDERR_FILENO, "L", 1);
+		prinInt(line_number);
+		write(STDERR_FILENO, ": unknown instruction ",
+		_strlen(": unknown instruction "));
+		write(STDERR_FILENO, cmd[0], _strlen(cmd[0]));
+		write(STDERR_FILENO, "\n", 1);
+		free_stack(h);
+		fclose(lt.file);
+		free_grid(cmd);
+		exit(EXIT_FAILURE);	
+}
+/**
+* print_push_error - ENTRYPOINT
+* @line_number: third param
+* Return: 0 success or uint
+*/
+print_push_error(int line_number)
+{
+	write(STDERR_FILENO, "L", 1);
+	prinInt(line_number);
+	write(STDERR_FILENO, ": usage: push integer\n",
+	_strlen(": usage: push integer\n"));
+	exit(EXIT_FAILURE);
+}
 /**
 * exec_cmd - ENTRYPOINT
 * @header: constant
@@ -25,31 +59,11 @@ void exec_cmd(stack_t **header, char **grid, int line_number)
 		if (grid && _strcmp(opcode_tab[index].opcode, grid[0]) == 0)
 		{
 			if (index == 0)
-			{
 				if (!grid[1])
-				{
-					write(STDERR_FILENO, "L", 2);
-					prinInt(line_number);
-					write(STDERR_FILENO, ": usage: push integer\n",
-					_strlen(": usage: push integer\n"));
-					exit(EXIT_FAILURE);
-				}
-			}
+					print_push_error(line_number);
 			opcode_tab[index].f(header, line_number);
 			break;
 		}
 	}
 	if (opcode_tab[index].f == NULL)
-	{
-		write(STDERR_FILENO, "L", 1);
-		prinInt(line_number);
-		write(STDERR_FILENO, ": unknown instruction ",
-		_strlen(": unknown instruction "));
-		write(STDERR_FILENO, grid[0], _strlen(grid[0]));
-		write(STDERR_FILENO, "\n", 1);
-		free_stack(*header);
-		fclose(lt.file);
-		free_grid(grid);
-		exit(EXIT_FAILURE);
-	}
-}
+		print_cmd_notfound(line_number, grid, *header);
